27332f9b103cf56b14989c310aa12a2676fc59ed
ecotiya <ghqyydf829@gmail.com>
2021-07-27 08:33:40 +0900

    update cors.rb

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -5,12 +5,18 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # 疎通を許可するリクエストオリジン
+    origins "localhost:5000" # React側はポート番号5000で作るので「localhost:5000」を指定
+
+    # 疎通を許可するリソースファイル
+    resource "*",
+      # 疎通を許可するHTTPヘッダー
+      headers: :any,
+      # 疎通を許可するHTTPメソッド(optionsはリクエスト許可チェックなので削除NG)
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      # Cookieを利用するか否か
+      # credentials: true
+  end
+end
