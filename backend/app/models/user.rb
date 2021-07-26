537d37ae83c6486d9225f64e8c7a74124a0a09b9
ecotiya <ghqyydf829@gmail.com>
2021-07-27 08:59:43 +0900

    device & device_token_authの設定更新

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
 
    # device機能説明
