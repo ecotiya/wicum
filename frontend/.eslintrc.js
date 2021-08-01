2366d9395f782a7fc3c81af57ec247e648db12d9
ecotiya <ghqyydf829@gmail.com>
2021-08-01 17:01:46 +0900

    eslintrc.js バグ修正

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,18 +1,20 @@
 module.exports = {
   env: {
-    browser: true,
-    es6: true
+    browser: true,  // ブラウザで実行されるコードを性的検証する。
+    es2020: true,
+    node: true  // module.exportsはnode.jsの書き方のため、envに追加しないとeslintに引っかかる。
   },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended", // TypeScriptでチェックされる項目をLintから除外する設定
-    "prettier", // prettierのextendsは他のextendsより後に記述する
+    "prettier" // prettierのextendsは他のextendsより後に記述する
   ],
   plugins: ["@typescript-eslint"],
-  parser: ["@typescript-eslint/parser"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    "sourceType": "module",
-    "project": "./tsconfig.json" // TypeScriptのLint時に参照するconfigファイルを指定
+    sourceType: "module",
+    ecmaVersion: 2020,
+    project: "./tsconfig.json" // TypeScriptのLint時に参照するconfigファイルを指定
   },
   root: true, // 上位ディレクトリにある他のeslintrcを参照しないようにする
   rules: {}
